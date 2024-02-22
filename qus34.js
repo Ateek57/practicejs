69b60550ef7291f7dd5c83b340cdd5cbc364d8c2
Ateek Ahamad <112778523+Ateek57@users.noreply.github.com>
2024-02-22 11:05:54 +0530

    Add files via upload

---
+++ qus34.js
@@ -0,0 +1,33 @@
+function createpromise(url){
+    return new Promise((resolve, reject) => {
+        console.log("started downloading the data",url)
+        setTimeout(() => {
+            resolve("xyz")
+            
+        },100);
+    })
+}
+function promise2(data){
+    return new Promise((resolve, reject) => {
+        console.log("started saving the data")
+        setTimeout(()=>{
+            if(Math.random()<0.5){
+                reject("data counld not saved")
+            }
+            else{
+                resolve("data is saved")
+            }
+        },500)
+    })
+}
+
+pr=createpromise("www.xyz.com")
+pr.then((value)=>{
+    console.log('data is downloaded successfully',value)
+    return promise2(value)
+})
+.then((value)=>{
+    console.log(value)
+}).catch((error)=>{
+    console.log(error)
+})
