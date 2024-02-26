60ad2f43984874acfad1788665ae22321dc59924
Ateek Ahamad <112778523+Ateek57@users.noreply.github.com>
2024-02-26 23:01:33 +0530

    Add files via upload

--- qus1.js
+++ qus1.js
@@ -1,6 +1,26 @@
-//Write a function that uses forEach to double each element in an array.
-let arr = [2,4,6,8]
-arr.forEach((element) =>{
+var isAnagram = function(s, t) {
+    if(s.length != t.length) {
+        return false;
+    }
     
-    console.log(element*element)
-})
+    const mp = {}
+    for(let i=0;i<s.length;i++){
+        if(mp[s[i]]){
+            mp[s[i]]+=1;//if it is defined, occurance found increse frequency
+        }else{
+            mp[s[i]]=1;//if it is undefined then make frequency count
+        }
+    }
+    for(let i=0;i<t.length;i++){
+        if(mp[t[i]]===undefined){
+            return false;
+        }//there is charcter in t which is not present in s
+        else{
+            mp[t[i]] -= 1 //decrese the frequency
+            if(mp[t[i]]==0){
+                delete mp[t[i]] //if frequency is 0 delete occurance;
+            }
+        }
+    }
+    return Object.keys(mp).length == 0;
+};
