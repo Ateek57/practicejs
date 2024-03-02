4aef8a3f7ce26a2d2d5296e5a98a007eefb0669b
Ateek Ahamad <112778523+Ateek57@users.noreply.github.com>
2024-03-02 15:11:25 +0530

    Update qus36.js

--- qus36.js
+++ qus36.js
@@ -1,8 +1,8 @@
 //reverse string 
-var reverseWords = function(s) {
+var reverseWord = function(s) {
     let total = s.split(" ")
     total = total.filter(total => total!= "") //this line check for every word of      string if string is empty then return flase otherwise return true; 
-    let n = total.length;
+    let m = total.length;
     let i = 0;
     let j = n-1;
     while(i<j){
