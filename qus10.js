66c93e3e10f5a1a2915b5a149ba29e5ed71ed411
Ateek Ahamad <112778523+Ateek57@users.noreply.github.com>
2024-03-07 15:56:19 +0530

    Update qus10.js

--- qus10.js
+++ qus10.js
@@ -1,7 +1,7 @@
 // qus11.  Use map to create a new array that contains the square of each number in a given array.
 let arr = [2,3,4,5]
-let result = arr.map((element)=>{
+let res = arr.map((element)=>{
     return element*element;
 
 })
-console.log(result)
+console.log(res)
