30606cd09be0dc279f1c2cbba66cee18c8eba082
Ateek Ahamad <112778523+Ateek57@users.noreply.github.com>
2024-03-13 12:13:14 +0530

    Update qus27.js

--- qus27.js
+++ qus27.js
@@ -1,9 +1,9 @@
-pr1 = new Promise((resolve, reject) => {
+pr = new Promise((resolve, reject) => {
     setTimeout(()=>{
         resolve("success")
 
     },100)
 })
-pr1.then(function handler(value){
+pr.then(function handler(value){
     console.log(value)
 })
