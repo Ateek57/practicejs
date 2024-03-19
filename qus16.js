ce4316830738ff09035801b5ebe301f94c3cf33a
Ateek Ahamad <112778523+Ateek57@users.noreply.github.com>
2024-03-19 22:22:07 +0530

    Update qus16.js

--- qus16.js
+++ qus16.js
@@ -1,6 +1,6 @@
 //qus16. Write a function that uses filter to create a new array with only the prime numbers from an existing array.
 let arr= [3, 4, 11, 12, 17]
-let result = arr.filter((element)=>{
+let res = arr.filter((element)=>{
     let count = 0;
     for(let i =1;i<=element; i++){
         if(element%i==0){
@@ -13,4 +13,4 @@ let result = arr.filter((element)=>{
     }
 })
 
-console.log(result)
+console.log(res)
