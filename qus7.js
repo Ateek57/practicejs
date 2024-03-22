19a3352a3971acd81c5bcf86d7c3063fd72aec73
Ateek Ahamad <112778523+Ateek57@users.noreply.github.com>
2024-03-22 18:08:39 +0530

    Update qus7.js

--- qus7.js
+++ qus7.js
@@ -1,7 +1,7 @@
 //qus7. Write a function that uses forEach to log only the even numbers from an array.
 
-let arr = [2,3,4,5,6]
-arr.forEach((element)=>{
+let array = [2,3,4,5,6]
+array.forEach((element)=>{
     if(element%2 == 0){
         console.log(element)
     }
