1e5194e13cefe1701e6aad68cd60b824ac91f3db
Ateek Ahamad <112778523+Ateek57@users.noreply.github.com>
2024-05-16 11:23:17 +0530

    Update qus12.js

--- qus12.js
+++ qus12.js
@@ -1,6 +1,6 @@
 //Qus12. Write a function that uses map to create an array of the lengths of strings in a given array.
-let arr = ['arav', 'ram','shyam']
-let res = arr.map((element)=>{
+let arraylist = ['arav', 'ram','shyam']
+let res = arraylist.map((element)=>{
     return element.length;
 })
 
